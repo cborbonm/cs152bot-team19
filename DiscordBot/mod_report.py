f063ccd6df4530d8497e120811eb8cfa75a37f1d
Ken.C <cabrerakalexis@gmail.com>
2023-05-23 16:24:00 -0700

    Mod report based of report.py

--- DiscordBot/mod_report.py
+++ DiscordBot/mod_report.py
@@ -3,23 +3,36 @@ import discord
 import re
 
 class State(Enum):
-    REPORT_START = auto()
-    AWAITING_MESSAGE = auto()
-    MESSAGE_IDENTIFIED = auto()
+    MOD_REPORT_START = auto()
+    AWAITING_FLAG = auto()
+    REVIEWING_REPORT = auto()
+    IMMEDIATE_DANGER = auto()
+    FINALIZE_OUTCOME = auto()
     REPORT_COMPLETE = auto()
     REPORT_CANCELLED = auto()
+
+
 
 class ModReport:
     START_KEYWORD = "modreport"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
 
+    AUTO_FLAGGING_KEYWORD = "af"
+
+    YES_KEYWORD = "yes"
+    NO_KEYWORD = "no"
+
     def __init__(self, report_num, client, author_id):
         self.report_num = report_num
-        self.state = State.REPORT_START
-        self.client = client
         self.author_id = author_id
+        self.state = State.MOD_REPORT_START
+        self.client = client
+        self.helpMessage = None
         self.message = None
+        self.source = None
+        self.reason = None
+        self.credibility = None
 
     def __str__(self) -> str:
         s = "--------------------------------------------------\n"
@@ -40,42 +53,44 @@ class ModReport:
         prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
         get you started and give you a model for working with Discord. 
         '''
-
-        if message.content == self.CANCEL_KEYWORD:
-            self.state = State.REPORT_COMPLETE
-            return ["Report cancelled."]
-        
-        if self.state == State.REPORT_START:
-            reply =  "Thank you for starting the reporting process. "
-            reply += "Say `help` at any time for more information.\n\n"
-            reply += "Please copy paste the link to the message you want to report.\n"
-            reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
-            self.state = State.AWAITING_MESSAGE
+        if self.state == State.MOD_REPORT_START:
+            reply = "Please review this report.\n"
+            if self.source == self.AUTO_FLAGGING_KEYWORD:
+                self.helpMessage = f"Can you tell who is the victim and attacker in this report? Please say `{self.YES_KEYWORD}` or `{self.NO_KEYWORD}`."
+                self.state = State.AWAITING_FLAG
+                return [reply, self.helpMessage]
+            self.state = State.REVIEWING_REPORT
             return [reply]
-        
-        if self.state == State.AWAITING_MESSAGE:
-            # Parse out the three ID strings from the message link
-            m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
-            if not m:
-                return ["I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."]
-            guild = self.client.get_guild(int(m.group(1)))
-            if not guild:
-                return ["I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."]
-            channel = guild.get_channel(int(m.group(2)))
-            if not channel:
-                return ["It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel."]
-            try:
-                message = await channel.fetch_message(int(m.group(3)))
-            except discord.errors.NotFound:
-                return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
 
-            # Here we've found the message - it's up to you to decide what to do next!
-            self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+        if self.state == State.AWAITING_FLAG:
+            if message.content.startswith(self.NO_KEYWORD):
+                return ["Please contact a specialized team"]
+            self.state = State.REVIEWING_REPORT
         
-        if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+        if self.state == State.REVIEWING_REPORT:
+            return [f"Is this a credible report? Please say  `{self.YES_KEYWORD}` or `{self.NO_KEYWORD}`."]
+        
+        if self.state == State.REVIEWING_REPORT:
+            if message.content.startswith(self.YES_KEYWORD):
+                self.credibility = True
+                self.state = State.IMMEDIATE_DANGER
+                return [f"Is the user in any immediate danger? Please say `{self.YES_KEYWORD}` or `{self.NO_KEYWORD}`."]
+            else:
+                self.credibility = False
+                self.state = State.FINALIZE_OUTCOME
+
+        if self.state == State.IMMEDIATE_DANGER:
+            self.state = State.FINALIZE_OUTCOME
+            if message.content.startswith(self.YES_KEYWORD):
+                return ["Please contact a specialized team to make a final outcome on this report."]
+            else:
+                return ["Contact the relevant stakeholders and give the attacker a warning/education."]
+        
+        if self.state == State.FINALIZE_OUTCOME:
+            if self.credibility == False:
+                return ["No action necessary."]
+            else:
+                return ["Decide on necessary actions."]
 
         return []
 
